735db78711c08ff6a217ad35e13d85c02dfc9827
Blueberry <snw.wht.lk@gmail.com>
2023-05-03 09:03:12 +0200

    started

--- src/main/java/com/epam/rd/autotasks/ConditionStatements.java
+++ src/main/java/com/epam/rd/autotasks/ConditionStatements.java
@@ -5,7 +5,20 @@ import java.rmi.UnexpectedException;
 class ConditionStatements {
 
     public static int task2(int n) {
-       //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+        int firstD, secondD, thirdD;
+        thirdD = n%10;
+        n = (n - n%10) / 10;
+        secondD = n %10;
+        n = (n - n%10) / 10;
+        firstD = n%10;
+
+        int newFirst = Math.max((Math.max(firstD, secondD)), thirdD);
+        int newThird = Math.min((Math.min(firstD, secondD)), thirdD);
+
+
+
+        int answer = newFirst*100 + newThird;
+
+        return 0;
     }
 }
